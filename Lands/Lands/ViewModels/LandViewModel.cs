c6c16961a3a81b7936ea9429d67bf50e375ef30f
fsegura58 <47571308+fsegura58@users.noreply.github.com>
2020-06-18 18:59:36 -0500

    Finish Land Tabbed Page

--- Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -9,6 +9,8 @@
     {
         #region Attributes
         private ObservableCollection<Border> borders;
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
 
         #region Properties
@@ -21,7 +23,19 @@
         public ObservableCollection<Border> Borders
         {
             get { return this.borders; }
-            set { SetValue(ref this.borders, value); }
+            set { this.SetValue(ref this.borders, value); }
+        }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
         }
         #endregion
 
@@ -30,6 +44,8 @@
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
         #endregion
 
