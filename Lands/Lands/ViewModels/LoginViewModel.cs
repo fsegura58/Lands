699db266cc13f83cde881b2e6b0ed17e3d7ff62d
fsegura58 <47571308+fsegura58@users.noreply.github.com>
2020-06-19 17:45:09 -0500

    Consume Token in Login

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -2,11 +2,16 @@
 {
     using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Services;
     using Views;
     using Xamarin.Forms;
 
     public class LoginViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
         #region Attributes
         private string email;
         private string password;
@@ -20,11 +25,13 @@
             get { return this.email; }
             set { SetValue(ref this.email, value); }
         }
+
         public string Password
         {
             get { return this.password; }
             set { SetValue(ref this.password, value); }
         }
+
         public bool IsRunning
         {
             get { return this.isRunning; }
@@ -35,6 +42,7 @@
             get;
             set;
         }
+
         public bool IsEnabled
         {
             get { return this.isEnabled; }
@@ -45,13 +53,13 @@
         #region Constructors
         public LoginViewModel()
         {
+            this.apiService = new ApiService();
+
             this.IsRemembered = true;
             this.IsEnabled = true;
 
-            this.Email = "fsegura58@gmail.com";
-            this.Password = "1234";
-
-            //http://restcountries.eu/rest/v2/all
+            //this.Email = "fsegura58@gmail.com";
+            //this.Password = "1234";
         }
         #endregion
 
@@ -74,6 +82,7 @@
                     "Accept");
                 return;
             }
+
             if (string.IsNullOrEmpty(this.Password))
             {
                 await Application.Current.MainPage.DisplayAlert(
@@ -86,26 +95,70 @@
             this.IsRunning = true;
             this.IsEnabled = false;
 
-            if (this.Email != "fsegura58@gmail.com" || this.Password != "1234")
+            //if (this.Email != "fsegura58@gmail.com" || this.Password != "1234")
+            //{
+            //    this.IsRunning = false;
+            //    this.IsEnabled = true;
+            //    await Application.Current.MainPage.DisplayAlert(
+            //        "Error",
+            //        "Email or password incorrect.",
+            //        "Accept");
+            //    this.Password = string.Empty;
+            //    return;
+            //}
+
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
             {
                 this.IsRunning = false;
                 this.IsEnabled = true;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
-                    "Email or password incorrect.",
+                    connection.Message,
+                    "Accept");
+                return;
+            }
+
+            var token = await this.apiService.GetToken(
+                "https://landsapi-1.azurewebsites.net", 
+                this.Email, 
+                this.Password);
+
+            if(token == null)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Sometime was wrong, please try later.",
+                    "Accept");
+                return;
+            }
+
+            if(string.IsNullOrEmpty(token.AccessToken))
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    token.ErrorDescription,
                     "Accept");
                 this.Password = string.Empty;
                 return;
             }
+
+            var mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token;
+
+            mainViewModel.Lands = new LandsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
 
             this.IsRunning = false;
             this.IsEnabled = true;
 
             this.Email = string.Empty;
             this.Password = string.Empty;
-
-            MainViewModel.GetInstance().Lands = new LandsViewModel();
-            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
         }
         #endregion
     }
